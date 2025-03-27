693236f7dfd8214544f0c7d7cd084243da30dfc7
devmet34 <devmet34@gmail.com>
2025-03-27 18:55:32 +0300

    Redis cache timeout lowered so fallback to DB can take place faster.

--- src/Web/todo.cs
+++ src/Web/todo.cs
@@ -29,8 +29,8 @@
 //todo add category to log output +
 //todo concurrency handling for orders +
 //todo optimize basket interface/services i.e; home/index get basket count only instead of whole basket for performance, getbasket calls with no tracking and always include all items, remove basket method?+
-//todo implement projections for Getbasket calls; basketvm>basketitemvm>productvm
-//todo basketvm to order instead of basket, basketvm to ordercontroller?
+//todo implement projections for Getbasket calls; basketvm>basketitemvm>productvm+
+//todo workaround for ef core concurrency/tracking issue with projections, e.g: order repo?
 //todo sec for all post actions
 //todo show specific error msgs when setbasketitem fails e.g; same qt already set
 //todo cleanup, sort warnings like unused assigments/usings, dereference possible nulls etc. 
